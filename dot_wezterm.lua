ee877f784fdf1d3d438d7bd7af2fde820da25d68
Thai Son <thaison@nexondv.com>
2025-06-17 18:12:41 +0700

    feat: save co configs

---
+++ dot_wezterm.lua
@@ -0,0 +1,229 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- config.leader = {key = '', mods = 'CTRL', timeout_miliseconds = 1000}
+
+config.keys = { -- config ActivatePaneDirection
+{
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Left'
+}, 
+{
+    key = 'h',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Left'
+
+},
+ {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Right'
+},
+ {
+    key = 'l',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Right'
+
+}, 
+{
+    key = 'UpArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Up'
+},
+ {
+    key = 'k',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Up'
+}
+, 
+{
+    key = 'DownArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Down'
+},
+ {
+    key = 'j',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Down'
+
+}, -- This will create a new split and run your default program inside it
+{
+    key = 'H',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitHorizontal {
+        domain = 'CurrentPaneDomain'
+    }
+}, -- This will create a new split and run your default program inside it
+{
+    key = 'V',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitVertical {
+        domain = 'CurrentPaneDomain'
+    }
+}, -- This will close current pane
+{
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane {
+        confirm = true
+    }
+}, -- This will rotate pane
+{
+    key = 'b',
+    mods = 'CTRL|ALT',
+    action = act.RotatePanes 'CounterClockwise'
+}, {
+    key = 'n',
+    mods = 'CTRL|ALT',
+    action = act.RotatePanes 'Clockwise'
+}, -- Clears the scrollback and viewport, and then sends CTRL-L to ask the
+-- shell to redraw its prompt
+{
+    key = 'K',
+    mods = 'CTRL|SHIFT',
+    action = act.Multiple {act.SendKey {
+        key = 'L',
+        mods = 'CTRL'
+    }}
+}, -- search for things that look like git hashes
+{
+    key = 'F',
+    mods = 'CTRL|SHIFT',
+    action = act.Search {
+        Regex = '[a-f0-9]{6,}'
+    }
+}, -- search for the lowercase string "" matching the case exactly
+{
+    key = 'F',
+    mods = 'CTRL|SHIFT',
+    action = act.Search {
+        CaseSensitiveString = ''
+    }
+}, -- search for the string "hash" matching regardless of case
+{
+    key = 'F',
+    mods = 'CTRL|SHIFT',
+    action = act.Search {
+        CaseInSensitiveString = ''
+    }
+}, -- Active next|previous tab
+{
+    key = '[',
+    mods = 'ALT',
+    action = act.ActivateTabRelative(-1)
+}, {
+    key = ']',
+    mods = 'ALT',
+    action = act.ActivateTabRelative(1)
+}, -- Move current tab relative
+{
+    key = '[',
+    mods = 'CTRL|ALT',
+    action = act.MoveTabRelative(-1)
+}, {
+    key = ']',
+    mods = 'CTRL|ALT',
+    action = act.MoveTabRelative(1)
+}}
+-- Override activeTab keybindings
+for i = 1, 9 do
+    -- CTRL+ALT + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = 'ALT',
+        action = act.ActivateTab(i - 1)
+    })
+    -- F1 through F8 to activate that tab
+    -- table.insert(config.keys, {
+    --     key = 'F' .. tostring(i),
+    --     action = act.ActivateTab(i - 1)
+    -- })
+end
+
+-- For example, changing the color scheme: Dark Violet (base16) | Argonaut (Gogh) | Blue Matrix | cyperpunk | Materia (base16) | Obsidian
+-- config.color_scheme = 'deep'
+-- config.color_scheme = 'Obsidian'
+
+-- colors, metadata = wezterm.color.load_scheme("wezterm-color-scheme/obsidian.toml")
+config.colors = colors
+
+-- config.colors = {
+--       ansi = {
+--     "#000000","#FF8225","#00bb00","#fecd22","#3a9bdb","#bb00bb","#00bbbb","#bbbbbb"
+--   },
+--   brights = {
+--    "#555555","#ff0003","#93c863","#fef874","#a1d7ff","#ff55ff","#55ffff","#ffffff"
+--   },
+-- }
+
+-- Appearance
+-- config.window_decorations = "RESIZE"
+config.tab_bar_at_bottom = true
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.underline_thickness = 1
+config.hide_mouse_cursor_when_typing = true
+config.use_fancy_tab_bar = false
+config.enable_scroll_bar = false
+config.font_size = 11
+-- config.command_palette_font_size = 10
+
+config.window_frame = {
+    font_size = 9,
+    -- The overall background color of the tab bar when
+    -- the window is focused
+    active_titlebar_bg = '#333333',
+
+    -- The overall background color of the tab bar when
+    -- the window is not focused
+    inactive_titlebar_bg = '#333333'
+
+}
+
+-- For font configs
+-- config.font = wezterm.font("Hack Nerd Font", {
+--     weight = "Bold",
+--     stretch = "Normal",
+--     style = "Normal"
+-- }) -- /usr/share/fonts/TTF/HackNerdFont-Regular.ttf, FontConfig
+-- config.font = wezterm.font_with_fallback {{
+--     family = "Hack Nerd Font",
+--     {
+--         weight = "Bold",
+--         stretch = "Normal",
+--         style = "Normal"
+--     }
+-- }, 'Consolas', 'Courier New', 'monospace'}
+
+config.window_background_opacity = 0.85
+
+-- For custom background image
+-- config.window_background_image = 'Downloads/wezterm_wallpaper.jpg'
+config.window_background_image_hsb = {
+    brightness = 0.5,
+    saturation = 0.8
+}
+
+config.inactive_pane_hsb = {
+    saturation = 0.8,
+    brightness = 0.5
+}
+-- Disable beep sound (bell rings) for laptop
+config.audible_bell = "Disabled"
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,34 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+if type -q eza
+    alias ls "eza -a --icons --header --time-style=default"
+    alias la "ls -G"
+    alias ll "ls -l"
+    alias lla "ll -A --tree"
+end
+alias vi nvim
+alias g git
+command -qv nvim && alias vim nvim
+## Node
+# Volta config
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Starship | https://starship.rs/
+# ~/.config/fish/config.fish
+starship init fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path /home/thaison/.spicetify
